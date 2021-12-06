bf97393f7bffac9dbe38b09f50861055894ae023
쳬 <silverrain0501@gmail.com>
2021-12-06 07:03:26 +0000

    일정 수정 기능 구현

--- juicy_board/urls.py
+++ juicy_board/urls.py
@@ -9,6 +9,8 @@ app_name = 'juicy_board'
 urlpatterns = [
     url(r'^$', views.Juicy_board.as_view(), name='juicy_board'),
     url(r'^insert/$', views.check_post, name="juicy_board_insert"),
+    url(r'^(?P<pk>[0-9]+)/detail/$', views.Juicy_board_detail.as_view(), name="juicy_board_detail"),
+    url(r'^(?P<pk>[0-9]+)/update/$', views.Juicy_board_update.as_view(), name="juicy_board_update"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- juicy_board/views.py
+++ juicy_board/views.py
@@ -26,4 +26,25 @@ def check_post(request):
         template_name = 'juicy_board/juicy_board_insert.html'
         form = JuicyForm
         return render(request, template_name, {"form":form})
+
+class Juicy_board_detail(generic.DetailView):
+    model = JuicyBoard
+    template_name = 'juicy_board/juicy_board_detail.html'
+    context_object_name = 'juicy_list'
     
+class Juicy_board_update(generic.UpdateView):
+    model = JuicyBoard
+    fields= {'title','content'}
+    template_name = "juicy_board/juicy_board_update.html"
+    success_url = '/board/'
+    
+    def form_valid(self, form):
+        form.save()
+        return render(self.request, 'juicy_board/juicy_board_success.html',{"message":'Juicy한 일정을 업데이트 했습니다.'})
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(object=self.object, form=form)
+        return self.render_to_response(context)
